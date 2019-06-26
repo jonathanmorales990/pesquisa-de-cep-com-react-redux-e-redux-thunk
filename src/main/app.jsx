db24063777c0469cc76c96a65ab5051eb206e415
Jonathan Morales <johnmorales990@gmail.com>
2019-06-26 14:11:48 -0300

    Update app.jsx

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-//mycomponents
+
 import Displaycep from '../components/displaycep';
 import Consultar from '../components/consultar';
-//redux import
 
 import { combineReducers, createStore, applyMiddleware  } from 'redux';
 import { Provider } from 'react-redux';
@@ -21,17 +20,15 @@ const store = createStore( reducers, applyMiddleware(thunk) );
 class App extends Component {
 	
 	render() {
-  	
-	    return (
-	    	<Provider store={store}> 
-		    	<div>
-		    		<Displaycep/>
-		    		<Consultar/>
-		    	</div>	
-	    	</Provider>
+		return (
+			<Provider store={store}> 
+				<div>
+					<Displaycep/>
+					<Consultar/>
+				</div>	
+			</Provider>
 		)
-	    
-  	}
+	}
 }
 
 export default App;
