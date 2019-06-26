df5170eeb4ba763d8f3c0eac382f4e3bd558cf65
Jonathan Morales <johnmorales990@gmail.com>
2019-06-26 14:05:50 -0300

    Update displaycep.jsx

--- src/components/displaycep.jsx
+++ src/components/displaycep.jsx
@@ -6,43 +6,37 @@ import { connect } from 'react-redux';
 
 class Displaycep extends Component {
 
-	/*componentDidUpdate(prevProps, prevState) {
-	    console.log(this.props.displaycep.consulta);
-	} */
 	render() {
-  	
-	    return (
+		return (
 			<Container>
-		    
 				<Row>
 					<Col className='displaycep' xs={{ size: 4, offset:4}} sm={{ size: 6, offset:4}} md={{ size: 4, offset:4 }}>
-
 						<Card body className="text-center" className='displaycepCard'>
 							<CardTitle>Consultar CEP</CardTitle>
 
-							{this.props.displaycep.erro ? 
-							<Alert color="danger">
-								<strong>Erro! </strong> {this.props.displaycep.erroMessage}
-							</Alert> : null
+							{
+								this.props.displaycep.erro ? 
+									<Alert color="danger">
+										<strong>Erro! </strong> {this.props.displaycep.erroMessage}
+									</Alert> : null
 							}
 
-							{this.props.displaycep.consulta ? 
-							<CardText>{`Bairro ${this.props.displaycep.consulta.bairro} ${this.props.displaycep.consulta.logradouro} ${this.props.displaycep.consulta.localidade} - ${this.props.displaycep.consulta.uf}`}</CardText> : null 
+							{
+								this.props.displaycep.consulta ? 
+									<CardText>{`Bairro ${this.props.displaycep.consulta.bairro} ${this.props.displaycep.consulta.logradouro} ${this.props.displaycep.consulta.localidade} - ${this.props.displaycep.consulta.uf}`}</CardText> : null 
 							}
 
-							{this.props.displaycep.loading ? 
-							<CardText>Procurando seu CEP aguarde...</CardText> : null 
+							{	
+								this.props.displaycep.loading ? 
+									<CardText>Procurando seu CEP aguarde...</CardText> : null 
 							}
 
 						</Card>
-
 					</Col>
 				</Row>
-		    
 			</Container>
 		)
-	    
-  	}
+	}
 }
 
 const mapStateToProps = state => ({ displaycep: state.displaycep });
